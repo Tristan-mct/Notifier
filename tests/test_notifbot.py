b46d8df4ded19e0575eacbd27c5dc6b5d88f6563
Tristan Muscat <tristan.muscat@gmail.com>
2021-11-21 13:05:20 +0100

    Modified test to allow it to fail with github action because of it needs a token to run.

--- tests/test_notifbot.py
+++ tests/test_notifbot.py
@@ -5,19 +5,17 @@ Created on Tue Aug 11 11:50:14 2020
 @author: Tristan Muscat
 """
 import unittest  # For unit testing
-from unittest.mock import patch
 
+import pytest
 from notifbot import NotifBot
 
 
 class TestNotifbot(unittest.TestCase):
-
+    @pytest.mark.xfail(reason="Fail in github action due to lacking token.")
     def test_readline(self):
         notifier = NotifBot()
-        str_channel = notifier.get_user_id('Tristan')
+        str_channel = notifier.get_user_id("Tristan")
         self.assertEqual(str_channel, "D02JZP30F0U")
-
-
 
 
 if __name__ == "__main__":
